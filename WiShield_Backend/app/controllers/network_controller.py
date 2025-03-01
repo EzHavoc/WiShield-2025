c005493d606b3c09947c626ebb87625bb290817d
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-03-01 14:56:23 +0530

    fix : deleted unnnescessary files

--- WiShield_Backend/app/controllers/network_controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.utils.network_scanner import scan_network
-from app.middleware.auth_middleware import auth_required
-
-router = APIRouter()
-
-@router.get("/scan", dependencies=[Depends(auth_required)])
-def scan():
-    return {"devices": scan_network()}
